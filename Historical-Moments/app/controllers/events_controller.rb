c195240840b628cd2c184a1939226dd7fb8ac77c
github username <github email address>
2019-08-19 18:02:57 +0100

    added delete to events and create moment to moments

--- Historical-Moments/app/controllers/events_controller.rb
+++ Historical-Moments/app/controllers/events_controller.rb
@@ -7,4 +7,38 @@ class EventsController < ApplicationController
     def show 
         @event = Event.find(params[:id])
     end 
+
+    def new
+        @event = Event.new
+    end 
+
+    def create
+        @event = Event.create(event_params)
+
+        redirect_to event_path(@event)
+    end 
+
+    def edit 
+        @event = Event.find(params[:id])
+    end
+
+    def update 
+        @event = Event.update(event_params)
+        @event.save
+
+        redirect_to event_path(@event)
+    end 
+
+    def destroy
+        Event.find(params[:id]).destroy
+
+        redirect_to events_path
+    end
+
+    private 
+
+    def event_params
+        params.require(:event).permit(:name, :description, :img_url, :event_date, :moment_id)
+    end
+
 end
