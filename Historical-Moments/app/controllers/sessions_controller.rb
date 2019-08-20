efcbedbf9e111afc97241e5aa29f091dffce4990
github username <github email address>
2019-08-20 15:28:53 +0100

    actor and document links on moments show page

---
+++ Historical-Moments/app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    render layout: false
+  end
+
+  def create
+    @user =  User.find_by(username: params[:user][:username])
+    if @user 
+      session[:user_id] = @user.id
+      redirect_to historical_moments_path
+    else
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+    session.destroy
+
+    redirect_to new_session_path
+  end
+end
