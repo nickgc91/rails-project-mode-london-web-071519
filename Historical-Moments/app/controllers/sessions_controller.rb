83ca083eeda1df62db9a4fb5e4307b73032fd59b
github username <github email address>
2019-08-20 18:30:56 +0100

    done with authentication

--- Historical-Moments/app/controllers/sessions_controller.rb
+++ Historical-Moments/app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to historical_moments_path
     else
+      flash[:notice] = "Sorry, we can't find a user with that username and password"
       redirect_to new_session_path
     end
   end
--- Historical-Moments/app/controllers/users_controller.rb
+++ Historical-Moments/app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
+        session[:user_id] = @user.id
+        
         redirect_to historical_moments_path
     end
 
