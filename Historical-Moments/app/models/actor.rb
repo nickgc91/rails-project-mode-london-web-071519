a8cfa982da17d0d25ba3aa54430332c1bfc07f92
github username <github email address>
2019-08-21 09:53:45 +0100

    created methods for events, actor, and moments

--- Historical-Moments/app/models/actor.rb
+++ Historical-Moments/app/models/actor.rb
@@ -1,4 +1,17 @@
 class Actor < ApplicationRecord
     has_many :locations 
     has_many :events, through: :locations
+
+
+    #analytics methods for Actor
+    def self.total_actors
+        Actor.all.count
+    end
+
+    def self.actor_with_most_events
+        Actor.all.max_by do |actor|
+            actor.events.count
+        end
+    end
+
 end
